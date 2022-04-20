6b8fb259262709b969b20253aad15ff9708bbdc6
Simone823 <simone.daglio823@outlook.it>
2022-04-20 23:09:28 +0200

    comic info, description wrapper, adv

--- resources/views/components/comicInfo.blade.php
+++ resources/views/components/comicInfo.blade.php
@@ -1,28 +1,57 @@
-@extends('layouts.default');
+@extends('layouts.default')
 
-@section('metaTitle', 'Comic Info');
+@section('metaTitle', 'Comic Info')
 
 
 @section('mainContent')
 
     <!-- Jumbotron -->
     <section class="jumbotron">
-        <div class="container">
-            {{-- <button>Current Series</button> --}}
+
+        {{-- Wrapper comic img  --}}
+        <div class="wrapper">
+
+            {{-- Comic img --}}
+            <figure class="comic_img">
+                <img src="{{$comic['thumb']}}" alt="">
+            </figure>
         </div>
+
     </section>
 
+    {{-- Banner blue --}}
+    <div class="banner_blue"></div>
+
     {{-- Container --}}
-    <div class="container">
-
-        @foreach ($comic as  $id => $element)
-
-            {{-- Comic description --}}
-            <div class="description_comic">
-                <h2>{{$element['description']}}</h2>
-            </div>
+    <div class="container_comic">
         
-        @endforeach
+        {{-- Comic description --}}
+        <div class="description_comic">
+            <h2>{{$comic['title']}}</h2>
+
+            {{-- Banner green --}}
+            <div class="banner_green">
+
+                {{-- Price wrapper --}}
+                <div class="price_wrapper">
+                    <p>U.S. Price: <span>{{$comic['price']}}</span></p>
+                    <p>Available</p>
+                </div>
+
+                {{-- Check availability --}}
+                <div class="available">
+                    <p>Check Availability</p>
+                </div>
+
+            </div>
+
+            <p>{{$comic['description']}}</p>
+        </div>
+
+        {{-- Adv --}}
+        <div class="adv">
+            <img src="{{asset('img/adv.jpg')}}" alt="">
+        </div>
     </div>
 
 @endsection
